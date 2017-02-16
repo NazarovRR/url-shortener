be7c86008255db367d3d2d14b83ca27d9944fe7b
Nazarov Mike <nazarovmichaelrr@gmail.com>
2017-02-16 19:00:32 +0300

    changed type of columnt full_url to get a big values

--- server/src/db/migrations/1486583977750_init.js
+++ server/src/db/migrations/1486583977750_init.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
 	//before the first start of the server, need to make first migration by knex:migrate latest, to create a columns
 	return knex.schema.createTable('urls', function(table) {
 		table.increments('id').primary();
-		table.string('full_url');
+		table.text('full_url');
 		table.string('encoded');
 		table.timestamps();
 	})
